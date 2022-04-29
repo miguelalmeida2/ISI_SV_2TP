7944c7bdd1aba4e374a0e7a51b893517c41beec8
Francisco Medeiros <91284180+Francisco-Medeiros00@users.noreply.github.com>
2022-04-30 00:34:10 +0100

    few changes in createTable

--- createTable.sql
+++ createTable.sql
@@ -1,82 +1,77 @@
-BEGIN transaction;
+--BEGIN transaction;
 
 --CASA_APOSTAS(id, nome, NIPC, aposta_minima)
 create table if not exists casa_apostas(
-	id 				INTEGER NOT NULL,
+	id 				serial,
 	nome 			varchar(150) NOT NULL,
 	NIPC 			varchar(9) NOT NULL,
-	aposta_minima 	real,
+	aposta_minima 	DECIMAL NOT NULL, --CHECK( aposta_minima SIMILAR TO '_%._%,')
 	PRIMARY KEY 	(id)
 );
 
 --ADMINISTRADOR(id, email, nome, perfil, casa_apostas)
 create table if not exists administrador(
-	id 				integer NOT NULL CHECK (id>0),
-	email 			varchar(60) Constraint admin_email UNIQUE (email) NOT NULL,
+	id 				integer NOT NULL CHECK, --(id>0)??,
+	email 			varchar(60) NOT NULL CHECK (email SIMILAR TO '_%@_%') Constraint admin_email UNIQUE (email),
 	nome 			varchar(150) NOT NULL,
 	perfil 			varchar(15) CHECK (perfil = 'admistrador' or perfil = 'supervisor' or perfil = 'operador'),
-	casa_apostas 	INTEGER,
-	PRIMARY KEY 	(id),
-	FOREIGN key 	(casa_apostas) REFERENCES casa_apostas (id)
-	);
+	casa_apostas 	INTEGER NOT NULL,
+	PRIMARY KEY 	(id)	
+);
 
 --APOSTA(transacao, tipo, odd, descricao)
 create table if not exists aposta(
 	transacao 		integer NOT NULL,
-	tipo 			varchar(15) CHECK (tipo = 'simples' or tipo = 'múltipla',
+	tipo 			varchar(15) CHECK (tipo = 'simples' or tipo = 'múltipla'),
 	odd 			integer CHECK (odd > 1),
 	descricao 		varchar(150),
-	PRIMARY KEY 	(transacao),
-	FOREIGN KEY 	(transacao)REFERENCES TRANSACAO (id)
+	PRIMARY KEY 	(transacao)
+	
 );
 
 
 --BANCARIA(transacao, operacao)
 create table if not exists bancaria(
 	transacao 		INTEGER NOT NULL,
-	operacao 		INTEGER CHECK (operacao = 'depósito' or operacao = 'levantamento'),
-	PRIMARY KEY 	(transacao),
-	FOREIGN KEY 	(transavao) REFERENCES TRANSACAO (numero)
-
+	operacao 		VARCHAR(15) CHECK (operacao = 'depósito' or operacao = 'levantamento'),
+	PRIMARY KEY 	(transacao)
+);
 
 --DOCUMENTO(jogador, numero, descricao, estado, data_registo)
 create table if not exists documento(
 	jogador 		INTEGER NOT NULL,
 	numero 			integer NOT NULL CHECK (numero > 0),
 	descricao 		varchar(150) NOT NULL,
-	estado 			varchar(15) CHECK(estado = 'pendente' or estado = 'aceite' or estado = 'recusado',
+	estado 			varchar(15) CHECK(estado = 'pendente' or estado = 'aceite' or estado = 'recusado'),
 	data_submissao 	date,
-	PRIMARY KEY 	(jogador, numero),
-	FOREIGN KEY 	(jogador) REFERENCES JOGADOR (id)
+	PRIMARY KEY 	(jogador, numero)
+	
 );
 
 --JOGADOR(id, email, nome, nickname, estado, data_nascimento, data_registo, morada, codigo_postal, localidade, casa_apostas)
 create table if not exists jogador(
 	id 				integer NOT NULL,
-	email 			varchar(60) Constraint jog_email UNIQUE (email) NOT NULL,
+	email 			varchar(60) Constraint jog_email UNIQUE (email) NOT NULL CHECK (email SIMILAR TO '_%@_%'),
 	nome 			varchar(150) NOT NULL,
 	nickname 		varchar(20)  Constraint player_name UNIQUE (nickname) NOT NULL,
 	estado 			varchar(15) DEFAULT 'activo',
-	data_nascimento date CHECK ( getdate - data_nascimento > 18) ,
-	data_registo 	date CHECK (data_registo - data_nascimento > 18 and data_registo < getdate())   ,
+	data_nascimento date CHECK ( CURRENT_DATE - data_nascimento > 18) ,
+	data_registo 	date CHECK (data_registo - data_nascimento > 18 and data_registo < CURRENT_DATE)  ,
 	morada 			varchar(150) NOT NULL,
-	codigo_posta	integer	NOT NULL,
+	codigo_posta	integer	NOT NULL CHECK (codpostal > 999999 AND codpostal < 10000000),
 	localidade 		varchar(50) NOT NULL,
 	casa_apostas 	integer NOT NULL,
-	PRIMARY KEY 	(id)
-	FOREIGN key 	(casa_apostas) REFERENCES casa_apostas (id)
-	
+	PRIMARY KEY 	(id)	
 );
 
 --RESOLUCAO(id, valor, resultado, data_resolucao, aposta)
 create table if not exists resolucao(
 	id 				integer NOT NULL,
 	valor 			real NOT NULL,
-	resultado 		varchar(15) CHECK (resultado = 'vitória' or resultado ='derrota' or resultado ='cashout' or resultado ='reembolso')
+	resultado 		varchar(15) CHECK (resultado = 'vitória' or resultado ='derrota' or resultado ='cashout' or resultado ='reembolso'),
 	data_resolucao 	date,
 	aposta 			integer NOT NULL,
-	PRIMARY KEY 	(id)
-	FOREIGN KEY 	(aposta) REFERENCES TRANSACAO (id)
+	PRIMARY KEY 	(id)	
 );
 
 --TRANSACAO(numero, valor, data_transacao, casa_apostas, jogador)
@@ -87,8 +82,23 @@ create table if not exists transacao(
 	casa_apostas 	integer NOT NULL,
 	jogador			integer NOT NULL,
 	PRIMARY KEY  	(numero)
-	FOREIGN KEY 	(casa_apostas) REFERENCES CASA_APOSTAS (id),
-	FOREIGN KEY  	(jogador) REFERENCES JOGADOR (id)
 );
 
-COMMIT transaction;
+ALTER TABLE administrador 
+	ADD CONSTRAINT casa_apostasFK FOREIGN key 	(casa_apostas) REFERENCES casa_apostas (id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE aposta 
+	ADD CONSTRAINT transacaoNumFk FOREIGN KEY 	(transacao)REFERENCES TRANSACAO (numero) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE bancaria 
+	ADD CONSTRAINT transacaoNumFk FOREIGN KEY 	(transacao)REFERENCES TRANSACAO (numero) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE documento 
+	ADD CONSTRAINT	jogadorIdFk FOREIGN KEY 	(jogador) REFERENCES JOGADOR (id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE jogador 
+	ADD CONSTRAINT	casa_apostasFK FOREIGN key 	(casa_apostas) REFERENCES casa_apostas (id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE resolucao 
+	ADD CONSTRAINT	transacaoNumFk FOREIGN KEY 	(aposta) REFERENCES TRANSACAO (numero) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE transacao 
+	ADD CONSTRAINT casa_apostasFK FOREIGN KEY 	(casa_apostas) REFERENCES CASA_APOSTAS (id)ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE transacao 
+	ADD CONSTRAINT jogadorIdFk FOREIGN KEY  	(jogador) REFERENCES JOGADOR (id) ON DELETE CASCADE ON UPDATE CASCADE;
+	
+--COMMIT transaction;
